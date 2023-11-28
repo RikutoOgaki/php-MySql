1b3298f97e47d6ff43ced67cea5b40d9e3467d7f
Rikuto <orikutobasuke@gmail.com>
2023-11-28 09:29:55 +0900

    index.phpの修正

--- html/index.php
+++ html/index.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>docekr php + mysql + phpmyadmin</title>
+    <title>Document</title>
 </head>
 <body>
-    <div>
-        <a href="./test.php">test.php</a>
-    </div>
+    <a href="./test.php">test.php</a>
 </body>
 </html>
