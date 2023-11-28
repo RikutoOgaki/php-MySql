f33854f82dcc7dcb6dd45ff2e8f80d7b3c26a0d3
Rikuto <orikutobasuke@gmail.com>
2023-11-28 12:19:52 +0900

    sample01追加

---
+++ html/sample01.php
@@ -0,0 +1,19 @@
+<?php
+    try{
+
+    }catch{
+
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sample01 データの取得</title>
+</head>
+<body>
+    
+</body>
+</html>
