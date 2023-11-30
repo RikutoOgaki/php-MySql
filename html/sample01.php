d583ea01c9ceae067a1ff40e998f1b08f205e20f
Rikuto <orikutobasuke@gmail.com>
2023-11-30 11:24:35 +0900

    mysqlに接続。テーブルのデータ取得。

--- html/sample01.php
+++ html/sample01.php
@@ -1,9 +1,32 @@
 <?php
-    try{
+$servername = "mysql"; // Docker Composeで設定したサービス名
+$username = "user"; // 環境変数 MYSQL_USER の値
+$password = "user"; // 環境変数 MYSQL_PASSWORD の値
+$dbname = "myDatabaseName"; // 環境変数 MYSQL_DATABASE の値
 
-    }catch{
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    }
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// ... (接続処理)
+
+$sql = "SELECT id,text  FROM my_table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["text"]. " - text: " . $row["text"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
 
 <!DOCTYPE html>
