d748290d328a36f8fb907d3fdc6540a9d079b5e5
Rikuto <orikutobasuke@gmail.com>
2023-12-04 14:40:31 +0900

    修正

--- html/index.php
+++ html/index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="./test.php">test.php</a>
-</body>
-</html>
+<?php
+    phpinfo()
+?>
